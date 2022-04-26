7912eba0c21a74d0a1e6c1a072bd106e41776715
aliwoto <woto@kaizoku.cyou>
2022-04-26 05:27:27 +0200

    Upgrade date in all comments headers
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

--- examples/bools/main.go
+++ examples/bools/main.go
@@ -1,5 +1,5 @@
 // argparser Project
-// Copyright (C) 2021 wotoTeam, ALiwoto
+// Copyright (C) 2021-2022 wotoTeam, ALiwoto
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of the source code.
 
